d510e1f759681eaa3cceefa3bf748ec807921f89
yuhao.wang <yuhao.wang@msxf.com>
2017-05-25 14:52:43 +0800

    修改项目名称

---
+++ spring-boot-student-config/src/main/java/com/xiaolyuh/config/XMLConfig.java
@@ -0,0 +1,10 @@
+package com.xiaolyuh.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource
+public class XMLConfig {
+
+}
