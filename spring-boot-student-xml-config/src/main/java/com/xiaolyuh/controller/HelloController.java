2dad5350562134b5ea4ad078fe9f69d70d2dd1be
yuhao.wang <yuhao.wang@msxf.com>
2017-05-25 11:12:36 +0800

    通过@ImportResource引入XML配置

--- spring-boot-student-xml-config/src/main/java/com/xiaolyuh/controller/HelloController.java
+++ spring-boot-student-xml-config/src/main/java/com/xiaolyuh/controller/HelloController.java
@@ -1,14 +1,20 @@
 package com.xiaolyuh.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.xiaolyuh.entity.Person;
 
 @RestController
 public class HelloController {
 	
+	@Autowired
+	private Person person;
+	
 	@GetMapping("index")
 	public Object index() {
 		
-		return "你好";
+		return person;
 	}
 }
