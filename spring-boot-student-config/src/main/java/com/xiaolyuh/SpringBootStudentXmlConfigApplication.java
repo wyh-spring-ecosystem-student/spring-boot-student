23123572e528e0736abf69deb58d8bdf86c2320b
yuhao.wang <yuhao.wang@msxf.com>
2017-05-25 14:58:45 +0800

    引入XML配置

--- spring-boot-student-config/src/main/java/com/xiaolyuh/SpringBootStudentXmlConfigApplication.java
+++ spring-boot-student-config/src/main/java/com/xiaolyuh/SpringBootStudentXmlConfigApplication.java
@@ -2,10 +2,8 @@ package com.xiaolyuh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath:config/some-context.xml"})
 public class SpringBootStudentXmlConfigApplication {
 
 	public static void main(String[] args) {
--- spring-boot-student-config/src/main/java/com/xiaolyuh/config/XMLConfig.java
+++ spring-boot-student-config/src/main/java/com/xiaolyuh/config/XMLConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource
+@ImportResource({"classpath:config/some-context.xml"})
 public class XMLConfig {
 
 }
