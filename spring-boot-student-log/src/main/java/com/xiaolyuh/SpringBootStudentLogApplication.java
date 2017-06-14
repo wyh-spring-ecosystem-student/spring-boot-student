7b8f14e873bbdff0d784d84a19c99034564343d6
yuhao.wang <yuhao.wang@msxf.com>
2017-06-14 16:29:40 +0800

    spring boot 日志

--- spring-boot-student-log/src/main/java/com/xiaolyuh/SpringBootStudentLogApplication.java
+++ spring-boot-student-log/src/main/java/com/xiaolyuh/SpringBootStudentLogApplication.java
@@ -1,22 +1,11 @@
 package com.xiaolyuh;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootStudentLogApplication {
-	private static final Logger logger = LoggerFactory.getLogger(SpringBootStudentLogApplication.class);
 	public static void main(String[] args) {
-		logger.debug("==============================================");
-		logger.debug("==============================================");
-		logger.debug("==============================================");
-		logger.debug("==============================================");
-		logger.debug("==============================================");
-		logger.debug("==============================================");
-		logger.debug("==============================================");
-		logger.debug("==============================================");
 		SpringApplication.run(SpringBootStudentLogApplication.class, args);
 	}
 }
