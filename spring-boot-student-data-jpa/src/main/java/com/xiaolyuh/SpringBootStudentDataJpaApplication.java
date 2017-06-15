d96bc692104ecb74ea61092b5c4d59274a594631
yuhao.wang <yuhao.wang@msxf.com>
2017-06-15 14:04:10 +0800

    Spring data Jpa

---
+++ spring-boot-student-data-jpa/src/main/java/com/xiaolyuh/SpringBootStudentDataJpaApplication.java
@@ -0,0 +1,12 @@
+package com.xiaolyuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudentDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudentDataJpaApplication.class, args);
+	}
+}
