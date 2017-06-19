722f6f0e1d4be3a54d11718eea9a17c8c1cdc1a9
yuhao.wang <yuhao.wang@msxf.com>
2017-06-19 11:33:33 +0800

    Spring Boot 配置 dbcp数据源

--- spring-boot-student-data-jpa/src/main/resources/data_1.sql
+++ spring-boot-student-data-jpa/src/main/resources/data_1.sql
@@ -1,5 +1,5 @@
 -- 文件名修改成data.sql才会执行
-insert into person(name,age,address) values('汪云飞',32,'合肥'); 
+insert into person(name,age,address) values('wyf',32,'合肥');
 insert into person(name,age,address) values('xx',31,'北京'); 
 insert into person(name,age,address) values('yy',30,'上海'); 
 insert into person(name,age,address) values('zz',29,'南京'); 
