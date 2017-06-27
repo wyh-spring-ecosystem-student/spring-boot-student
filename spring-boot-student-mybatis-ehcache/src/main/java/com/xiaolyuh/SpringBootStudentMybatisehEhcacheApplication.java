ef21f580f7c33d1eae7208172e7bc86de9ec3302
yuhao.wang <yuhao.wang@msxf.com>
2017-06-27 19:15:51 +0800

    Mybatis 使用EhCache缓存

---
+++ spring-boot-student-mybatis-ehcache/src/main/java/com/xiaolyuh/SpringBootStudentMybatisehEhcacheApplication.java
@@ -0,0 +1,12 @@
+package com.xiaolyuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudentMybatisehEhcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudentMybatisehEhcacheApplication.class, args);
+	}
+}
