d644b5d315b64b6ec6793346706031c2c3342d20
yuhao.wang <yuhao.wang@msxf.com>
2017-06-30 17:53:20 +0800

    配置Druid的监控页面

---
+++ spring-boot-student-mybatis-druid/src/test/java/com/xiaolyuh/DataSourceTests.java
@@ -0,0 +1,35 @@
+package com.xiaolyuh;
+
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.annotation.JacksonStdImpl;
+import com.xiaolyuh.config.DruidDataSourceProperty;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataSourceTests {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    DruidDataSourceProperty druidDataSourceProperty;
+
+    @Test
+    public void testDataSource() throws Exception {
+        // 获取配置的数据源
+        DataSource dataSource = applicationContext.getBean(DataSource.class);
+        // 查看配置数据源信息
+        System.out.println(dataSource);
+        System.out.println(dataSource.getClass().getName());
+        System.out.println(JSON.toJSONString(druidDataSourceProperty));
+    }
+
+}
