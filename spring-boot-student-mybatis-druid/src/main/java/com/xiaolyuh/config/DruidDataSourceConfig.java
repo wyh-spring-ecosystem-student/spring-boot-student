6a078c06180926fe48876a72da87ae030015ee46
yuhao.wang <yuhao.wang@msxf.com>
2017-06-30 17:32:13 +0800

    Mybatis 配置阿里的Druid数据库连接池

---
+++ spring-boot-student-mybatis-druid/src/main/java/com/xiaolyuh/config/DruidDataSourceConfig.java
@@ -0,0 +1,59 @@
+package com.xiaolyuh.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+@ConditionalOnClass(com.alibaba.druid.pool.DruidDataSource.class)
+@ConditionalOnProperty(name = "spring.datasource.type", havingValue = "com.alibaba.druid.pool.DruidDataSource", matchIfMissing = true)
+public class DruidDataSourceConfig {
+
+    private Logger logger = LoggerFactory.getLogger(DruidDataSourceConfig.class);
+
+    @Autowired
+    private DruidDataSourceProperty druidDataSourceProperty;
+
+
+    @Bean     //声明其为Bean实例
+    @Primary  //在同样的DataSource中，首先使用被标注的DataSource
+    public DataSource dataSource(){
+        DruidDataSource datasource = new DruidDataSource();
+
+        datasource.setUrl(druidDataSourceProperty.getUrl());
+        datasource.setUsername(druidDataSourceProperty.getUsername());
+        datasource.setPassword(druidDataSourceProperty.getPassword());
+        datasource.setDriverClassName(druidDataSourceProperty.getDriverClassName());
+
+        //configuration
+        datasource.setInitialSize(druidDataSourceProperty.getInitialSize());
+        datasource.setMinIdle(druidDataSourceProperty.getMinIdle());
+        datasource.setMaxActive(druidDataSourceProperty.getMaxActive());
+        datasource.setMaxWait(druidDataSourceProperty.getMaxWait());
+        datasource.setTimeBetweenEvictionRunsMillis(druidDataSourceProperty.getTimeBetweenEvictionRunsMillis());
+        datasource.setMinEvictableIdleTimeMillis(druidDataSourceProperty.getMinEvictableIdleTimeMillis());
+        datasource.setValidationQuery(druidDataSourceProperty.getValidationQuery());
+        datasource.setTestWhileIdle(druidDataSourceProperty.isTestWhileIdle());
+        datasource.setTestOnBorrow(druidDataSourceProperty.isTestOnBorrow());
+        datasource.setTestOnReturn(druidDataSourceProperty.isTestOnReturn());
+        datasource.setPoolPreparedStatements(druidDataSourceProperty.isPoolPreparedStatements());
+        datasource.setMaxPoolPreparedStatementPerConnectionSize(druidDataSourceProperty.getMaxPoolPreparedStatementPerConnectionSize());
+        try {
+            datasource.setFilters(druidDataSourceProperty.getFilters());
+        } catch (SQLException e) {
+            logger.error("druid configuration initialization filter", e);
+        }
+        datasource.setConnectionProperties(druidDataSourceProperty.getConnectionProperties());
+
+        return datasource;
+    }
+}  
---
+++ spring-boot-student-mybatis-druid/src/main/java/com/xiaolyuh/config/DruidDataSourceProperty.java
@@ -0,0 +1,188 @@
+package com.xiaolyuh.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "spring.datasource.druid")
+public class DruidDataSourceProperty {
+	private String url;
+
+	private String username;
+
+	private String password;
+
+	private String driverClassName;
+
+	private int initialSize = 0;
+
+	private int minIdle;
+
+	private int maxActive = 8;
+
+	private int maxWait;
+
+	private int timeBetweenEvictionRunsMillis = 1000 * 60;
+
+	private int minEvictableIdleTimeMillis = 1000 * 60 * 30;
+
+	private String validationQuery;
+
+	private boolean testWhileIdle = false;
+
+	private boolean testOnBorrow = true;
+
+	private boolean testOnReturn = false;
+
+	private boolean poolPreparedStatements = false;
+
+	private int maxPoolPreparedStatementPerConnectionSize = - 1;
+
+	private String filters;
+
+	private String connectionProperties;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+
+	public int getInitialSize() {
+		return initialSize;
+	}
+
+	public void setInitialSize(int initialSize) {
+		this.initialSize = initialSize;
+	}
+
+	public int getMinIdle() {
+		return minIdle;
+	}
+
+	public void setMinIdle(int minIdle) {
+		this.minIdle = minIdle;
+	}
+
+	public int getMaxActive() {
+		return maxActive;
+	}
+
+	public void setMaxActive(int maxActive) {
+		this.maxActive = maxActive;
+	}
+
+	public int getMaxWait() {
+		return maxWait;
+	}
+
+	public void setMaxWait(int maxWait) {
+		this.maxWait = maxWait;
+	}
+
+	public int getTimeBetweenEvictionRunsMillis() {
+		return timeBetweenEvictionRunsMillis;
+	}
+
+	public void setTimeBetweenEvictionRunsMillis(int timeBetweenEvictionRunsMillis) {
+		this.timeBetweenEvictionRunsMillis = timeBetweenEvictionRunsMillis;
+	}
+
+	public int getMinEvictableIdleTimeMillis() {
+		return minEvictableIdleTimeMillis;
+	}
+
+	public void setMinEvictableIdleTimeMillis(int minEvictableIdleTimeMillis) {
+		this.minEvictableIdleTimeMillis = minEvictableIdleTimeMillis;
+	}
+
+	public String getValidationQuery() {
+		return validationQuery;
+	}
+
+	public void setValidationQuery(String validationQuery) {
+		this.validationQuery = validationQuery;
+	}
+
+	public boolean isTestWhileIdle() {
+		return testWhileIdle;
+	}
+
+	public void setTestWhileIdle(boolean testWhileIdle) {
+		this.testWhileIdle = testWhileIdle;
+	}
+
+	public boolean isTestOnBorrow() {
+		return testOnBorrow;
+	}
+
+	public void setTestOnBorrow(boolean testOnBorrow) {
+		this.testOnBorrow = testOnBorrow;
+	}
+
+	public boolean isTestOnReturn() {
+		return testOnReturn;
+	}
+
+	public void setTestOnReturn(boolean testOnReturn) {
+		this.testOnReturn = testOnReturn;
+	}
+
+	public boolean isPoolPreparedStatements() {
+		return poolPreparedStatements;
+	}
+
+	public void setPoolPreparedStatements(boolean poolPreparedStatements) {
+		this.poolPreparedStatements = poolPreparedStatements;
+	}
+
+	public int getMaxPoolPreparedStatementPerConnectionSize() {
+		return maxPoolPreparedStatementPerConnectionSize;
+	}
+
+	public void setMaxPoolPreparedStatementPerConnectionSize(int maxPoolPreparedStatementPerConnectionSize) {
+		this.maxPoolPreparedStatementPerConnectionSize = maxPoolPreparedStatementPerConnectionSize;
+	}
+
+	public String getFilters() {
+		return filters;
+	}
+
+	public void setFilters(String filters) {
+		this.filters = filters;
+	}
+
+	public String getConnectionProperties() {
+		return connectionProperties;
+	}
+
+	public void setConnectionProperties(String connectionProperties) {
+		this.connectionProperties = connectionProperties;
+	}
+}
