b1756975c3df507666d3340c1fb90ccccbd4d6dd
wyhmonstar <yuhao.wang@msxf.com>
2017-08-09 11:27:55 +0800

    Spring 对事务的支持

---
+++ spring-boot-student-data-jpa-transaction/src/main/java/com/xiaolyuh/SpringBootStudentDataJpaTransactionApplication.java
@@ -0,0 +1,12 @@
+package com.xiaolyuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudentDataJpaTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudentDataJpaTransactionApplication.class, args);
+	}
+}
