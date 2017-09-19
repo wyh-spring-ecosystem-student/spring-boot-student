d781a918462c15ddf64efe51368502dba120b3a3
wyhmonstar <yuhao.wang@msxf.com>
2017-09-19 17:08:12 +0800

    Spring Boot Mongo

---
+++ spring-boot-student-data-mongo/src/main/java/com/xiaolyuh/entity/Location.java
@@ -0,0 +1,38 @@
+package com.xiaolyuh.entity;
+
+public class Location {
+
+    private String place;
+	
+	private String year;
+	
+	
+
+	public Location(String place, String year) {
+		super();
+		this.place = place;
+		this.year = year;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+
+	
+
+	
+
+}
