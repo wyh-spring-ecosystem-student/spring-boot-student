afd2614e02c0a2795720ae95e35b0977aa8656c5
wyhmonstar <xiaolyuh@163.com>
2017-12-05 13:41:30 +0800

    caffeine 缓存

---
+++ spring-boot-student-cache-caffeine/src/main/java/com/xiaolyuh/config/CacheConfig.java
@@ -0,0 +1,19 @@
+package com.xiaolyuh.config;
+
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yuhao.wang
+ */
+@Configuration
+public class CacheConfig {
+
+//    @Bean
+//    public CaffeineCacheManager cacheManager() {
+//        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+//        return caffeineCacheManager;
+//    }
+
+}
