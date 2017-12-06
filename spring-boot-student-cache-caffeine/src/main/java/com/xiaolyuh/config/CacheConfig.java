5230d3c5b71607b825fe7f5a50c77de373270012
wyhmonstar <xiaolyuh@163.com>
2017-12-06 13:33:31 +0800

    加入 CacheLoader

--- spring-boot-student-cache-caffeine/src/main/java/com/xiaolyuh/config/CacheConfig.java
+++ spring-boot-student-cache-caffeine/src/main/java/com/xiaolyuh/config/CacheConfig.java
@@ -1,5 +1,7 @@
 package com.xiaolyuh.config;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -8,12 +10,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CacheConfig {
 
-//    @Bean
-//    public CaffeineCacheManager cacheManager() {
-//        SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
-//        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-//
-//        return caffeineCacheManager;
-//    }
+    /**
+     * 必须要指定这个Bean，refreshAfterWrite=5s这个配置属性才生效
+     *
+     * @return
+     */
+    @Bean
+    public CacheLoader<Object, Object> cacheLoader() {
+
+
+        CacheLoader<Object, Object> cacheLoader = new CacheLoader<Object, Object>() {
+
+            @Override
+            public Object load(Object key) throws Exception {
+                return null;
+            }
+
+            // 重写这个方法将oldValue值返回回去，进而刷新缓存
+            @Override
+            public Object reload(Object key, Object oldValue) throws Exception {
+                return oldValue;
+            }
+        };
+
+        return cacheLoader;
+    }
 
 }
