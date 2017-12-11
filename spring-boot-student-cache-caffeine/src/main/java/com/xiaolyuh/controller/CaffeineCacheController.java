b74eeb28aceb0be58d6ba870efb2ff89db2f719b
wyhmonstar <xiaolyuh@163.com>
2017-12-11 18:12:48 +0800

    基于时间（Time-based）

--- spring-boot-student-cache-caffeine/src/main/java/com/xiaolyuh/controller/CaffeineCacheController.java
+++ spring-boot-student-cache-caffeine/src/main/java/com/xiaolyuh/controller/CaffeineCacheController.java
@@ -1,6 +1,7 @@
 package com.xiaolyuh.controller;
 
 import com.github.benmanes.caffeine.cache.*;
+import com.google.common.testing.FakeTicker;
 import com.xiaolyuh.entity.Person;
 import com.xiaolyuh.service.PersonService;
 import org.apache.commons.lang3.time.DateUtils;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -124,9 +126,13 @@ public class CaffeineCacheController {
     @RequestMapping("/testTimeBased")
     public Object testTimeBased(Person person) {
         String key = "name1";
+        // 用户测试，一个时间源，返回一个时间值，表示从某个固定但任意时间点开始经过的纳秒数。
+        FakeTicker ticker = new FakeTicker();
+
         // 基于固定的到期策略进行退出
         // expireAfterAccess
         LoadingCache<String, Object> graphs1 = Caffeine.newBuilder()
+                .ticker(ticker::read)
                 .expireAfterAccess(5, TimeUnit.SECONDS)
                 .build(k -> createExpensiveGraph(k));
 
@@ -134,15 +140,17 @@ public class CaffeineCacheController {
         graphs1.get(key);
 
         System.out.println("expireAfterAccess：等待4.9S后，第二次次获取缓存");
-        threadSleep(4900L);
+        // 直接指定时钟
+        ticker.advance(4900, TimeUnit.MILLISECONDS);
         graphs1.get(key);
 
         System.out.println("expireAfterAccess：等待0.101S后，第三次次获取缓存");
-        threadSleep(101L);
+        ticker.advance(101, TimeUnit.MILLISECONDS);
         graphs1.get(key);
 
         // expireAfterWrite
         LoadingCache<String, Object> graphs2 = Caffeine.newBuilder()
+                .ticker(ticker::read)
                 .expireAfterWrite(5, TimeUnit.SECONDS)
                 .build(k -> createExpensiveGraph(k));
 
@@ -150,31 +158,30 @@ public class CaffeineCacheController {
         graphs2.get(key);
 
         System.out.println("expireAfterWrite：等待4.9S后，第二次次获取缓存");
-        threadSleep(4900L);
+        ticker.advance(4900, TimeUnit.MILLISECONDS);
         graphs2.get(key);
 
         System.out.println("expireAfterWrite：等待0.101S后，第三次次获取缓存");
-        threadSleep(101L);
+        ticker.advance(101, TimeUnit.MILLISECONDS);
         graphs2.get(key);
 
         // Evict based on a varying expiration policy
         // 基于不同的到期策略进行退出
         LoadingCache<String, Object> graphs3 = Caffeine.newBuilder()
+                .ticker(ticker::read)
                 .expireAfter(new Expiry<String, Object>() {
-                    @Override
-                    public long expireAfterCreate(String key, Object graph, long currentTime) {
 
+                    @Override
+                    public long expireAfterCreate(String key, Object value, long currentTime) {
                         // Use wall clock time, rather than nanotime, if from an external resource
-                        long seconds = DateUtils.addSeconds(new Date(), 5).getTime();
-                        System.out.println("调用了 expireAfterCreate方法：" + TimeUnit.MILLISECONDS.toNanos(seconds));
-                        return TimeUnit.MILLISECONDS.toNanos(seconds);
+                        return TimeUnit.SECONDS.toNanos(5);
                     }
 
                     @Override
                     public long expireAfterUpdate(String key, Object graph,
                                                   long currentTime, long currentDuration) {
 
-                        System.out.println("调用了 expireAfterUpdate：" + currentDuration);
+                        System.out.println("调用了 expireAfterUpdate：" + TimeUnit.NANOSECONDS.toMillis(currentDuration));
                         return currentDuration;
                     }
 
@@ -182,7 +189,7 @@ public class CaffeineCacheController {
                     public long expireAfterRead(String key, Object graph,
                                                 long currentTime, long currentDuration) {
 
-                        System.out.println("调用了 expireAfterRead：" + currentDuration);
+                        System.out.println("调用了 expireAfterRead：" + TimeUnit.NANOSECONDS.toMillis(currentDuration));
                         return currentDuration;
                     }
                 })
@@ -192,21 +199,14 @@ public class CaffeineCacheController {
         graphs3.get(key);
 
         System.out.println("expireAfter：等待4.9S后，第二次次获取缓存");
-        threadSleep(4900L);
+        ticker.advance(4900, TimeUnit.MILLISECONDS);
         graphs3.get(key);
 
         System.out.println("expireAfter：等待0.101S后，第三次次获取缓存");
-        threadSleep(101L);
+        ticker.advance(101, TimeUnit.MILLISECONDS);
         Object object = graphs3.get(key);
 
         return object;
     }
 
-    private void threadSleep(long time) {
-        try {
-            Thread.sleep(time);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
