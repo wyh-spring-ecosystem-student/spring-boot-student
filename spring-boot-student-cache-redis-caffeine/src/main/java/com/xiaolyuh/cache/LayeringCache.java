86d200a50fcd67c25c2182d00abc0d125bcafc90
yuhao.wang <yuhao.wang@msxf.com>
2017-12-14 21:12:19 +0800

    添加redis配置

--- spring-boot-student-cache-redis-caffeine/src/main/java/com/xiaolyuh/cache/LayeringCache.java
+++ spring-boot-student-cache-redis-caffeine/src/main/java/com/xiaolyuh/cache/LayeringCache.java
@@ -1,16 +1,13 @@
 package com.xiaolyuh.cache;
 
-import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.Cache;
+import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.AbstractValueAdaptingCache;
-import org.springframework.cache.support.SimpleValueWrapper;
 import org.springframework.data.redis.cache.RedisCache;
 import org.springframework.data.redis.core.RedisOperations;
 
 import java.util.concurrent.Callable;
-import java.util.function.Function;
 
 /**
  * @author yuhao.wang
@@ -27,15 +24,14 @@ public class LayeringCache extends AbstractValueAdaptingCache {
     /**
      * redi缓存
      */
-    private final RedisCache redisCache;
+    private RedisCache redisCache;
 
     /**
      * Caffeine缓存
      */
-    private final com.github.benmanes.caffeine.cache.Cache<Object, Object> caffeineCache;
+    private CaffeineCache caffeineCache;
 
-    public LayeringCache(String name, RedisCache redisCache, com.github.benmanes.caffeine.cache.Cache<Object, Object> caffeineCache,
-                         boolean allowNullValues) {
+    public LayeringCache(String name, RedisCache redisCache, CaffeineCache caffeineCache, boolean allowNullValues) {
 
         super(allowNullValues);
         this.name = name;
@@ -44,16 +40,16 @@ public class LayeringCache extends AbstractValueAdaptingCache {
     }
 
     public LayeringCache(String name, byte[] prefix, RedisOperations<? extends Object, ? extends Object> redisOperations,
-                         long expiration, com.github.benmanes.caffeine.cache.Cache<Object, Object> caffeineCache) {
-        this(name, prefix, redisOperations, expiration, false, caffeineCache);
+                         long expiration, com.github.benmanes.caffeine.cache.Cache<Object, Object> cache) {
+        this(name, prefix, redisOperations, expiration, false, cache);
     }
 
     public LayeringCache(String name, byte[] prefix, RedisOperations<? extends Object, ? extends Object> redisOperations,
-                         long expiration, boolean allowNullValues, com.github.benmanes.caffeine.cache.Cache<Object, Object> caffeineCache) {
+                         long expiration, boolean allowNullValues, com.github.benmanes.caffeine.cache.Cache<Object, Object> cache) {
         super(allowNullValues);
         this.name = name;
         this.redisCache = new RedisCache(name, prefix, redisOperations, expiration, allowNullValues);
-        this.caffeineCache = caffeineCache;
+        this.caffeineCache = new CaffeineCache(name, cache, allowNullValues);
     }
 
     @Override
@@ -69,7 +65,7 @@ public class LayeringCache extends AbstractValueAdaptingCache {
     @Override
     public ValueWrapper get(Object key) {
         // 查询一级缓存
-        ValueWrapper wrapper = toValueWrapper(caffeineCache.get(key, k -> createCaffeineCache(k)));
+        ValueWrapper wrapper = caffeineCache.get(key);
         logger.debug("查询一级缓存 key:{},返回值是:{}", key, wrapper);
         if (wrapper == null) {
             // 查询二级缓存
@@ -82,143 +78,60 @@ public class LayeringCache extends AbstractValueAdaptingCache {
     @Override
     public <T> T get(Object key, Class<T> type) {
         // 查询一级缓存
-//        T t = caffeineCache.get(key, type);
-        T t = null;
-        logger.debug("查询一级缓存 key:{},返回值是:{}", key, t);
-        if (t == null) {
+        T value = caffeineCache.get(key, type);
+        logger.debug("查询一级缓存 key:{},返回值是:{}", key, value);
+        if (value == null) {
             // 查询二级缓存
-            t = redisCache.get(key, type);
-            logger.debug("查询二级缓存 key:{},返回值是:{}", key, t);
+            value = redisCache.get(key, type);
+            logger.debug("查询二级缓存 key:{},返回值是:{}", key, value);
         }
-        return t;
+        return value;
     }
 
     @Override
     public <T> T get(Object key, Callable<T> valueLoader) {
         // 查询一级缓存
-//        T t = caffeineCache.get(key, k -> createCaffeineCache(k));
-        T t = null;
-        logger.debug("查询一级缓存 key:{},返回值是:{}", key, t);
-        if (t == null) {
+        T value = (T) caffeineCache.get(key);
+        logger.debug("查询一级缓存 key:{},返回值是:{}", key, value);
+        if (value == null) {
             // 查询二级缓存
-            t = redisCache.get(key, valueLoader);
+            value = redisCache.get(key, valueLoader);
         }
-        return t;
+        return value;
     }
 
     @Override
     public void put(Object key, Object value) {
-        caffeineCache.put(key, toStoreValue(value));
+        caffeineCache.put(key, value);
         redisCache.put(key, value);
     }
 
     @Override
     public ValueWrapper putIfAbsent(Object key, Object value) {
-        putIfAbsentCaffeineCache(key, value);
+        caffeineCache.putIfAbsent(key, value);
         return redisCache.putIfAbsent(key, value);
     }
 
     @Override
     public void evict(Object key) {
-        caffeineCache.invalidate(key);
+        caffeineCache.evict(key);
         redisCache.evict(key);
     }
 
     @Override
     public void clear() {
-        caffeineCache.invalidateAll();
+        caffeineCache.clear();
         redisCache.clear();
     }
 
     @Override
     protected Object lookup(Object key) {
-        Object object = caffeineCache.get(key, k -> createCaffeineCache(k));
-        if (object == null) {
-            object = redisCache.get(key);
+        Object value = caffeineCache.get(key);
+        logger.debug("查询一级缓存 key:{},返回值是:{}", key, value);
+        if (value == null) {
+            value = redisCache.get(key);
+            logger.debug("查询二级缓存 key:{},返回值是:{}", key, value);
         }
-        return object;
+        return value;
     }
-
-    private Object createCaffeineCache(Object key) {
-        return redisCache.get(key);
-    }
-
-    @Override
-    protected Cache.ValueWrapper toValueWrapper(Object storeValue) {
-        return (storeValue != null ? new SimpleValueWrapper(fromStoreValue(storeValue)) : null);
-    }
-
-
-
-
-
-
-//    @Override
-//    public ValueWrapper get(Object key) {
-//        if (this.cache instanceof LoadingCache) {
-//            Object value = ((LoadingCache<Object, Object>) this.cache).get(key);
-//            return toValueWrapper(value);
-//        }
-//        return super.get(key);
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    @Override
-//    public <T> T get(Object key, final Callable<T> valueLoader) {
-//        return (T) fromStoreValue(this.cache.get(key, new CaffeineCache.LoadFunction(valueLoader)));
-//    }
-//
-//    @Override
-//    protected Object lookup(Object key) {
-//        return this.cache.getIfPresent(key);
-//    }
-
-    private ValueWrapper putIfAbsentCaffeineCache(Object key, final Object value) {
-        LayeringCache.PutIfAbsentCaffeineCacheFunction callable = new LayeringCache.PutIfAbsentCaffeineCacheFunction(value);
-        Object result = this.caffeineCache.get(key, callable);
-        return (callable.called ? null : toValueWrapper(result));
-    }
-
-    private class PutIfAbsentCaffeineCacheFunction implements Function<Object, Object> {
-
-        private final Object value;
-
-        private boolean called;
-
-        public PutIfAbsentCaffeineCacheFunction(Object value) {
-            this.value = value;
-        }
-
-        @Override
-        public Object apply(Object key) {
-            this.called = true;
-            return toStoreValue(this.value);
-        }
-    }
-
-
-    private class LoadFunction implements Function<Object, Object> {
-
-        private final Callable<?> valueLoader;
-
-        public LoadFunction(Callable<?> valueLoader) {
-            this.valueLoader = valueLoader;
-        }
-
-        @Override
-        public Object apply(Object o) {
-            try {
-                return toStoreValue(valueLoader.call());
-            }
-            catch (Exception ex) {
-                throw new ValueRetrievalException(o, valueLoader, ex);
-            }
-        }
-    }
-
-
-
-
-
-
 }
