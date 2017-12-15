86e4dbb98e93726f6bf9e539d8dd051567fad938
wyhmonstar <xiaolyuh@163.com>
2017-12-15 10:44:48 +0800

    包名优化

--- spring-boot-student-cache-redis-caffeine/src/main/java/com/xiaolyuh/controller/CaffeineCacheController.java
+++ spring-boot-student-cache-redis-caffeine/src/main/java/com/xiaolyuh/controller/CaffeineCacheController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.OptionalLong;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executor;
