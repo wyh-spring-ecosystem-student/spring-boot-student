42114d5819b33cf5e8adb361af77927a077b4f80
wyhmonstar <xiaolyuh@163.com>
2017-12-21 16:30:05 +0800

    修改测试数据

--- spring-boot-student-cache-redis-2/src/main/java/com/xiaolyuh/redis/config/RedisConfig.java
+++ spring-boot-student-cache-redis-2/src/main/java/com/xiaolyuh/redis/config/RedisConfig.java
@@ -35,7 +35,7 @@ public class RedisConfig {
     @Value("${select.cache.timeout:1800}")
     private long selectCacheTimeout;
     // 查询缓存自动刷新时间
-    @Value("${select.cache.refresh:1799}")
+    @Value("${select.cache.refresh:1790}")
     private long selectCacheRefresh;
 
     /**
