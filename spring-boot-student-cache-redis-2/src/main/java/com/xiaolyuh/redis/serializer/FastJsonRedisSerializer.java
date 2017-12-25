9ef781b78dba414abb7da911e64ae441181e5535
wyhmonstar <xiaolyuh@163.com>
2017-12-25 16:08:06 +0800

    修改序列化判断

--- spring-boot-student-cache-redis-2/src/main/java/com/xiaolyuh/redis/serializer/FastJsonRedisSerializer.java
+++ spring-boot-student-cache-redis-2/src/main/java/com/xiaolyuh/redis/serializer/FastJsonRedisSerializer.java
@@ -2,6 +2,7 @@ package com.xiaolyuh.redis.serializer;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.springframework.cache.support.NullValue;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
@@ -20,7 +21,7 @@ public class FastJsonRedisSerializer<T> implements RedisSerializer<T> {
 
     @Override
     public byte[] serialize(T t) throws SerializationException {
-        if (t == null) {
+        if (t == null || t instanceof NullValue) {
             return new byte[0];
         }
         return JSON.toJSONString(t, SerializerFeature.WriteClassName).getBytes(DEFAULT_CHARSET);
