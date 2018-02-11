6caf12a9f10189fbfe55abc1bcc14761bebbe0ee
wyhmonstar <xiaolyuh@163.com>
2018-02-11 14:53:28 +0800

    添加Docker 云部署测试类

---
+++ spring-boot-student-banner/src/main/java/com/xiaolyuh/MainController.java
@@ -0,0 +1,18 @@
+package com.xiaolyuh;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yuhao.wang
+ */
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+
+        return "Hello World";
+    }
+}
