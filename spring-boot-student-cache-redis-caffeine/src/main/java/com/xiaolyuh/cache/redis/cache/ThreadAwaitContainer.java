f32e057110c3c71339bb9ac30be17cfb68c7c1d9
wyhmonstar <xiaolyuh@163.com>
2018-03-14 17:45:07 +0800

    缓存优化

--- spring-boot-student-cache-redis-caffeine/src/main/java/com/xiaolyuh/cache/redis/cache/ThreadAwaitContainer.java
+++ spring-boot-student-cache-redis-caffeine/src/main/java/com/xiaolyuh/cache/redis/cache/ThreadAwaitContainer.java
@@ -23,14 +23,17 @@ public class ThreadAwaitContainer {
      * @throws InterruptedException
      */
     public final void await(String key, long milliseconds) throws InterruptedException {
+        // 测试当前线程是否已经被中断
         if (Thread.interrupted()) {
             throw new InterruptedException();
         }
         Set<Thread> threadSet = threadMap.get(key);
+        // 判断线程容器是否是null，如果是就新创建一个
         if (threadSet == null) {
             threadSet = new HashSet<>();
             threadMap.put(key, threadSet);
         }
+        // 将线程放到容器
         threadSet.add(Thread.currentThread());
         // 阻塞一定的时间
         LockSupport.parkNanos(this, TimeUnit.MILLISECONDS.toNanos(milliseconds));
@@ -42,10 +45,13 @@ public class ThreadAwaitContainer {
      */
     public final void signalAll(String key) {
         Set<Thread> threadSet = threadMap.get(key);
+        // 判断key所对应的等待线程容器是否是null
         if (!CollectionUtils.isEmpty(threadSet)) {
             for (Thread thread : threadSet) {
                 LockSupport.unpark(thread);
             }
+            // 清空等待线程容器
+            threadSet.clear();
         }
     }
 
