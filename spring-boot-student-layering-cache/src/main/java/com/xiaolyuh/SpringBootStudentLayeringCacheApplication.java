6290e8990b75a2558352fdccd998b018d580cbff
wyhmonstar <xiaolyuh@163.com>
2018-07-30 21:19:48 +0800

    多级缓存start

--- spring-boot-student-layering-cache/src/main/java/com/xiaolyuh/SpringBootStudentLayeringCacheApplication.java
+++ spring-boot-student-layering-cache/src/main/java/com/xiaolyuh/SpringBootStudentLayeringCacheApplication.java
@@ -1,11 +1,11 @@
 package com.xiaolyuh;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching// 开启缓存，需要显示的指定
 public class SpringBootStudentLayeringCacheApplication {
 
     public static void main(String[] args) {
