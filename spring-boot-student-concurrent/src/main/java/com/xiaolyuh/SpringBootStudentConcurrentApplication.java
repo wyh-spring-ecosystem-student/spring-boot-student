2756c51f390e2b5d345920e3e2f966442af5f413
wyhmonstar <xiaolyuh@163.com>
2019-03-19 09:29:41 +0800

    并发编程

---
+++ spring-boot-student-concurrent/src/main/java/com/xiaolyuh/SpringBootStudentConcurrentApplication.java
@@ -0,0 +1,12 @@
+package com.xiaolyuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudentConcurrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudentConcurrentApplication.class, args);
+    }
+}
---
+++ spring-boot-student-concurrent/src/test/java/com/xiaolyuh/SpringBootStudentConcurrentApplicationTests.java
@@ -0,0 +1,57 @@
+package com.xiaolyuh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootStudentConcurrentApplicationTests {
+
+    @Test
+    public void runnableTest() {
+        // JDK 1.7
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "::Runnable异步任务1");
+            }
+        }).start();
+
+        // JDK 1.8
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + "::Runnable异步任务2")).start();
+    }
+
+    @Test
+    public void callableTest() {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        // JDK 1.7
+        FutureTask<String> futureTask1 = new FutureTask<>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return Thread.currentThread().getName() + "::Callable异步任务1";
+            }
+        });
+
+        // JDK 1.8
+        FutureTask<String> futureTask2 = new FutureTask<>(() -> Thread.currentThread().getName() + "::Callable异步任务2");
+
+        service.submit(futureTask1);
+        service.submit(futureTask2);
+        try {
+            System.out.println(futureTask1.get(5, TimeUnit.SECONDS));
+            System.out.println(futureTask2.get(5, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
