48c7b91e41d3519afa2e7ea10f894977ff72b0cf
wyhmonstar <xiaolyuh@163.com>
2019-12-30 18:17:28 +0800

    方法区内存溢出

--- spring-boot-student-okhttp/src/main/java/com/xiaolyuh/SpringBootStudentOkhttpApplication.java
+++ spring-boot-student-okhttp/src/main/java/com/xiaolyuh/SpringBootStudentOkhttpApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootStudentOkhttpApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootStudentValidatedApplication.class, args);
+        SpringApplication.run(SpringBootStudentOkhttpApplication.class, args);
     }
 }
