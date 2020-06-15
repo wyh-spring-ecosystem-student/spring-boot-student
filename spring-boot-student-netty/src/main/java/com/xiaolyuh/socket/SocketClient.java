fa9703c05d40d5f3c354b089d6a78543073378c9
olafwang <olafwang@tencent.com>
2020-06-15 19:01:43 +0800

    Socket 示例

--- spring-boot-student-netty/src/main/java/com/xiaolyuh/socket/SocketClient.java
+++ spring-boot-student-netty/src/main/java/com/xiaolyuh/socket/SocketClient.java
@@ -16,17 +16,34 @@ public class SocketClient {
 
     public static void main(String[] args) throws IOException {
         // host即客户端需要连接的机器，port就是服务器端用来监听请求的端口
-        String host = "127.0.0.1";
-        int port = 9999;
-        try {
-            Socket client = new Socket(host, port);
-            Writer writer = new OutputStreamWriter(client.getOutputStream());
-            writer.write("Hello Socket");
-            writer.flush();
-            writer.close();
-            client.close();
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
+        try{
+            //向本机的4700端口发出客户请求
+            Socket socket=new Socket("127.0.0.1",4700);
+            //由系统标准输入设备构造BufferedReader对象
+            BufferedReader sin=new BufferedReader(new InputStreamReader(System.in));
+            //由Socket对象得到输出流，并构造PrintWriter对象
+            PrintWriter os=new PrintWriter(socket.getOutputStream());
+            //由Socket对象得到输入流，并构造相应的BufferedReader对象
+            BufferedReader is=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String readline=sin.readLine(); //从系统标准输入读入一字符串
+            //若从标准输入读入的字符串为 "bye"则停止循环
+            while(!readline.equals("bye")){
+                //将从系统标准输入读入的字符串输出到Server
+                os.println(readline);
+                //刷新输出流，使Server马上收到该字符串
+                os.flush();
+                //在系统标准输出上打印读入的字符串
+                System.out.println("Client:"+readline);
+                //从Server读入一字符串，并打印到标准输出上
+                System.out.println("Server:"+is.readLine());
+                readline=sin.readLine(); //从系统标准输入读入一字符串
+            }
+            os.close(); //关闭Socket输出流
+            is.close(); //关闭Socket输入流
+            socket.close(); //关闭Socket
+        }catch(Exception e){
+            e.printStackTrace();//出错，打印出错信息
         }
+
     }
 }
--- spring-boot-student-netty/src/main/java/com/xiaolyuh/socket/SocketServer.java
+++ spring-boot-student-netty/src/main/java/com/xiaolyuh/socket/SocketServer.java
@@ -2,12 +2,11 @@ package com.xiaolyuh.socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,27 +19,46 @@ public class SocketServer {
 
     public static void main(String[] args) throws IOException {
         try {
-            // 开启有个服务端的Socket，在选择端口时，需要注意一点，就是0~1023这些端口都已经被系统预留了。
-            ServerSocket echoServer = new ServerSocket(9999);
-            // 从ServerSocket创建一个Socket对象用来监听和接受连接。
-            Socket clientSocket = echoServer.accept();
-            BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            // 应答客户端
-            PrintStream os = new PrintStream(clientSocket.getOutputStream());
+            //创建一个ServerSocket在端口4700监听客户请求
+            ServerSocket server = new ServerSocket(4700);;
+
+            //使用accept()阻塞等待客户请求，有客户, 请求到来则产生一个Socket对象，并继续执行
+            Socket socket = server.accept();;
+
             String line;
-            // 将数据回现客户端
-            while (true) {
-                line = br.readLine();
-                if (!StringUtils.isEmpty(line)) {
-                    System.out.println("收到客户端数据：" + line);
-                    os.println("服务端的响应数据：" + line);
-                    os.close();
-                    br.close();
-                    clientSocket.close();
-                    break;
-                }
+            //由Socket对象得到输入流，并构造相应的BufferedReader对象
+            BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //由Socket对象得到输出流，并构造PrintWriter对象
+            PrintWriter os = new PrintWriter(socket.getOutputStream());
+            //由系统标准输入设备构造BufferedReader对象
+            BufferedReader sin = new BufferedReader(new InputStreamReader(System.in));
+            //在标准输出上打印从客户端读入的字符串
+            System.out.println("Client:" + is.readLine());
+            //从标准输入读入一字符串
+            line = sin.readLine();
+            //如果该字符串为 "bye"，则停止循环
+            while (!line.equals("bye")) {
+                //向客户端输出该字符串
+                os.println(line);
+                //刷新输出流，使Client马上收到该字符串
+                os.flush();
+                //在系统标准输出上打印读入的字符串
+                System.out.println("Server:" + line);
+                //从Client读入一字符串，并打印到标准输出上
+                System.out.println("Client:" + is.readLine());
+                //从系统标准输入读入一字符串
+                line = sin.readLine();
             }
-        } catch (IOException e) {
+
+            //关闭Socket输出流
+            os.close();
+            //关闭Socket输入流
+            is.close();
+            //关闭Socket
+            socket.close();
+            //关闭ServerSocket
+            server.close();
+        } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
     }
