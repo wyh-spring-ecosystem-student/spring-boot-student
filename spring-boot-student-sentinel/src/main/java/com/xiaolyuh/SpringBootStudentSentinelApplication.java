7afac119de7640e40612d8b7dc51af1f5893168e
olafwang <olafwang@tencent.com>
2020-08-11 21:36:59 +0800

    添加注释

--- spring-boot-student-sentinel/src/main/java/com/xiaolyuh/SpringBootStudentSentinelApplication.java
+++ spring-boot-student-sentinel/src/main/java/com/xiaolyuh/SpringBootStudentSentinelApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootStudentSentinelApplication {
 
     public static void main(String[] args) {
+        // -Dcsp.sentinel.dashboard.server=127.0.0.1:8900 -Dproject.name=sentinel-demo
         SpringApplication.run(SpringBootStudentSentinelApplication.class, args);
     }
 }
