ac7e46a5f75a63caff01b6dc97103d7374cdd303
olafwang <olafwang@tencent.com>
2020-09-29 14:40:12 +0800

    原生 Kafka

---
+++ spring-boot-student-kafka/src/main/java/com/xiaolyuh/MainController.java
@@ -0,0 +1,17 @@
+package com.xiaolyuh;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yuhao.wang
+ */
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+
+        return "Hello World";
+    }
+}
