4f1d61c7d11a4338e5099a68665531802637b32e
olafwang <olafwang@tencent.com>
2020-12-29 16:49:13 +0800

    文件导出

--- spring-boot-student-export/src/main/java/com/xiaolyuh/utils/CsvExportUtil.java
+++ spring-boot-student-export/src/main/java/com/xiaolyuh/utils/CsvExportUtil.java
@@ -2,20 +2,20 @@ package com.xiaolyuh.utils;
 
 import com.google.common.net.HttpHeaders;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 导出CSV
+ *
+ * @author olafwang
  */
 public class CsvExportUtil {
     private static final String CSV_COLUMN_SEPARATOR = ",";
@@ -24,29 +24,26 @@ public class CsvExportUtil {
 
     public static void doExportByList(List<List<Object>> dataList, List<String> titles, OutputStream os) throws IOException {
         StringBuffer buf = new StringBuffer();
-        Iterator var4;
         if (CollectionUtils.isNotEmpty(titles)) {
-            var4 = titles.iterator();
-
-            while (var4.hasNext()) {
-                String title = (String) var4.next();
-                buf.append(title).append(",");
+            for (int i = 0; i < titles.size(); i++) {
+                String title = titles.get(i);
+                buf.append(title);
+                if (i < titles.size() - 1) {
+                    buf.append(CSV_TAB).append(CSV_COLUMN_SEPARATOR);
+                }
             }
-
-            buf.append("\r\n");
+            buf.append(CSV_ROW_SEPARATOR);
         }
 
         if (CollectionUtils.isNotEmpty(dataList)) {
-            var4 = dataList.iterator();
-
-            while (var4.hasNext()) {
-                List<Object> data = (List) var4.next();
-
-                for (int i = 0; i < data.size(); ++i) {
-                    buf.append(data.get(i)).append("\t").append(",");
+            for (List<Object> data : dataList) {
+                for (int i = 0; i < data.size(); i++) {
+                    buf.append(data.get(i));
+                    if (i < titles.size() - 1) {
+                        buf.append(CSV_TAB).append(CSV_COLUMN_SEPARATOR);
+                    }
                 }
-
-                buf.append("\r\n");
+                buf.append(CSV_ROW_SEPARATOR);
             }
         }
 
@@ -54,40 +51,10 @@ public class CsvExportUtil {
         os.flush();
     }
 
-    public static void doExport(List<Map<String, Object>> dataList, List<String> titles, List<String> keys, OutputStream os) throws Exception {
-        StringBuffer buf = new StringBuffer();
-        Iterator var5 = titles.iterator();
-
-        while (var5.hasNext()) {
-            String title = (String) var5.next();
-            buf.append(title).append(",");
-        }
-
-        buf.append("\r\n");
-        if (CollectionUtils.isNotEmpty(dataList)) {
-            var5 = dataList.iterator();
-
-            while (var5.hasNext()) {
-                Map<String, Object> data = (Map) var5.next();
-                Iterator var7 = keys.iterator();
-
-                while (var7.hasNext()) {
-                    String key = (String) var7.next();
-                    buf.append(data.get(key)).append(",");
-                }
-
-                buf.append("\r\n");
-            }
-        }
-
-        os.write(buf.toString().getBytes("GBK"));
-    }
-
     public static void responseSetProperties(String fileName, HttpServletResponse response) throws UnsupportedEncodingException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
         String fn = fileName == null ? sdf.format(new Date()) + ".csv" : fileName + sdf.format(new Date()) + ".csv";
         String utf = "UTF-8";
-        //response.setContentType("application/ms-txt.numberformat:@");
         response.setCharacterEncoding(utf);
         response.setHeader("Pragma", "public");
         response.setHeader("Cache-Control", "max-age=30");
@@ -98,28 +65,4 @@ public class CsvExportUtil {
         response.setHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
     }
 
-    public static void setResponseHeader(String fileName, HttpServletResponse response) throws UnsupportedEncodingException {
-        String utf = "UTF-8";
-        response.setContentType("application/octet-stream");
-        response.setCharacterEncoding(utf);
-        response.setHeader("Pragma", "public");
-        response.setHeader("Cache-Control", "max-age=30");
-        response.setHeader("Content-Disposition", "attachment; filename=" + URLEncoder.encode(fileName, utf));
-    }
-
-    public static InputStream genInputStream(List<List<Object>> dataList, List<String> titles) {
-        StringBuilder sb = new StringBuilder(StringUtils.join(titles, ","));
-        sb.append("\r\n");
-        Iterator var3 = dataList.iterator();
-
-        while (var3.hasNext()) {
-            List<Object> objects = (List) var3.next();
-            sb.append(StringUtils.join(objects, ",")).append("\r\n");
-        }
-
-        String csv = sb.toString();
-        byte[] bytes = csv.getBytes(StandardCharsets.UTF_8);
-        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-        return bais;
-    }
 }
