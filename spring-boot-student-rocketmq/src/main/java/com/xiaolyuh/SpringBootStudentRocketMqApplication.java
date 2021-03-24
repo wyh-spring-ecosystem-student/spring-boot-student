0b9869e4f1a53fbaaeb8bccde79c8f28611f01d3
wangyuhao <wangyuhao@fiture.com>
2021-03-24 09:55:41 +0800

    RocketMQ INIT

---
+++ spring-boot-student-rocketmq/src/main/java/com/xiaolyuh/SpringBootStudentRocketMqApplication.java
@@ -0,0 +1,22 @@
+package com.xiaolyuh;
+
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudentRocketMqApplication {
+
+    // 标准的JAVA应用main方法，主要作用作为项目启动的入口
+    public static void main(String[] args) {
+//		SpringApplication.run(SpringBootStudentBannerApplication.class, args);
+        SpringApplication application = new SpringApplication(SpringBootStudentExportApplication.class);
+        /*
+         * Mode.OFF:关闭;
+         * Mode.CONSOLE:控制台输出，默认方式;
+         * Mode.LOG:日志输出方式;
+         */
+        application.setBannerMode(Mode.CONSOLE);
+        application.run(args);
+    }
+}
