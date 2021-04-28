c6435fb37aac30f8df22cc564e7c1c62c12be5a9
wangyuhao01@longfor.com <xiaolyuh@163.com>
2021-04-28 09:19:56 +0800

    重试

--- spring-boot-student-guava-retrying/src/main/java/com/xiaolyuh/service/RetryService.java
+++ spring-boot-student-guava-retrying/src/main/java/com/xiaolyuh/service/RetryService.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class RetryService {
-    @Retryable(exception = Throwable.class, attemptNumber = 4, sleepTime = 300, timeUnit = TimeUnit.MILLISECONDS, retryListener = MyRetryListener.class)
+    @Retryable(exception = Throwable.class, attemptNumber = 4, sleepTime = 500, timeUnit = TimeUnit.MILLISECONDS, retryListener = MyRetryListener.class)
     public Object retry() {
         System.out.println("测试方法重试");
 
