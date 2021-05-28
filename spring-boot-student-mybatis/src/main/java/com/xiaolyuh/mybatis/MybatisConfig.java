94e48ccf9cb9e887162ead406dc51d7f1e7e38c8
wangyuhao01@longfor.com <wangyuhao01@longfor.com>
2021-05-28 15:33:25 +0800

    Mybatis 插件开发

--- spring-boot-student-mybatis/src/main/java/com/xiaolyuh/mybatis/MybatisConfig.java
+++ spring-boot-student-mybatis/src/main/java/com/xiaolyuh/mybatis/MybatisConfig.java
@@ -1,17 +1,16 @@
 package com.xiaolyuh.mybatis;
 
-import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MybatisConfig {
 
-//    // 注册插件方式1
-//    @Bean
-//    public EditPlugin myPlugin() {
-//        return new EditPlugin();
-//    }
+    // 注册插件方式1
+    @Bean
+    public EditPlugin myPlugin() {
+        return new EditPlugin();
+    }
 //
 //
 //    // 注册插件方式2
@@ -27,13 +26,13 @@ public class MybatisConfig {
 //        };
 //    }
 
-    // 注册插件方式2
-    @Bean
-    public ConfigurationCustomizer configurationCustomizer() {
-        return configuration -> {
-            //插件拦截链采用了责任链模式，执行顺序和加入连接链的顺序有关
-            EditPlugin myPlugin = new EditPlugin();
-            configuration.addInterceptor(myPlugin);
-        };
-    }
+//    // 注册插件方式2
+//    @Bean
+//    public ConfigurationCustomizer configurationCustomizer() {
+//        return configuration -> {
+//            //插件拦截链采用了责任链模式，执行顺序和加入连接链的顺序有关
+//            EditPlugin myPlugin = new EditPlugin();
+//            configuration.addInterceptor(myPlugin);
+//        };
+//    }
 }
